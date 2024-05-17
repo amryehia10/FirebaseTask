967a07c34ae93e55870b4b33472e8569de2670bd
Amr Yehia <amr.yehiaz100@gmail.com>
2024-05-17 21:44:19 +0300

    first commit

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,134 @@
+import 'package:email_validator/email_validator.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:user_auth/screens/home.dart';
+import 'package:user_auth/screens/register_screen.dart';
+
+class Login extends StatefulWidget {
+  const Login({super.key});
+
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  
+  final formKey = GlobalKey<FormState>();
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Form(
+        key: formKey,
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  "Register",
+                  style: TextStyle(
+                    fontSize: 30,
+                    color: Colors.deepPurple
+                  ),
+                ),
+                TextFormField(
+                  decoration: const InputDecoration(label: Text("Email")),
+                  controller: emailController,
+                  validator: (value) {
+                    bool isValid = EmailValidator.validate(value!);
+                    if(!isValid || value.isEmpty) {
+                      return "Email or Password is invalid";
+                    } else {
+                      return null;
+                    }
+                  },
+                ),
+                const SizedBox(height: 20,),
+                TextFormField(
+                  decoration: const InputDecoration(label: Text("Password")),
+                  controller: passwordController,
+                  obscureText: true,
+                  validator: (value) {
+                    if(value!.length < 6) {
+                      return "Email or Password is invalid";
+                    } else {
+                      return null;
+                    }
+                  },
+                ),
+                const SizedBox(height: 20,),
+                ElevatedButton(onPressed: ()=>{
+                  if(formKey.currentState!.validate()){
+                      userLogin()
+                    }
+                }, child: const Text("Login")),
+                const SizedBox(height: 20,),
+                Row(
+                  children: [
+                    const Text("Doesn't have an account?"),
+                    TextButton(onPressed: ()=>{
+                      Navigator.push(context,MaterialPageRoute<void>(
+                        builder: (BuildContext context) => const Register(),
+                      ),)
+                    }, child: const Text("Register here"))
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+  Future<void>userLogin() async {
+    showDialog(context: context, builder: (context) => const Center(
+        child: CircularProgressIndicator(),
+      )
+    );
+
+    try {
+      await FirebaseAuth.instance.signInWithEmailAndPassword(email: emailController.text, password: passwordController.text);
+      if(mounted) {
+      Navigator.push(context, 
+          MaterialPageRoute<void>(builder: (BuildContext context) => const Home(),),);
+      
+      Fluttertoast.showToast(
+            msg: "Login Successfully",
+            toastLength: Toast.LENGTH_SHORT,
+            gravity: ToastGravity.CENTER,
+            timeInSecForIosWeb: 1,
+            backgroundColor: Colors.deepPurple,
+            textColor: Colors.white,
+            fontSize: 16.0
+         );
+      }
+      
+    }on FirebaseException catch(e) {
+      if(mounted) {
+        Navigator.pop(context);
+        Fluttertoast.showToast(
+            msg: e.code,
+            toastLength: Toast.LENGTH_SHORT,
+            gravity: ToastGravity.CENTER,
+            timeInSecForIosWeb: 1,
+            backgroundColor: Colors.deepPurple,
+            textColor: Colors.white,
+            fontSize: 16.0
+         );
+      }
+      }
+    }
+
+}
---
+++ lib/screens/register_screen.dart
@@ -0,0 +1,155 @@
+import 'package:email_validator/email_validator.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:user_auth/screens/login_screen.dart';
+
+class Register extends StatefulWidget {
+  const Register({super.key});
+
+  @override
+  State<Register> createState() => _RegisterState();
+}
+
+class _RegisterState extends State<Register> {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  TextEditingController confirmPasswordController = TextEditingController();
+  TextEditingController nameController = TextEditingController();
+  final formKey = GlobalKey<FormState>();
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    passwordController.dispose();
+    confirmPasswordController.dispose();
+    nameController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Form(
+          key: formKey,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    "Register",
+                    style: TextStyle(
+                      fontSize: 30,
+                      color: Colors.deepPurple
+                    ),
+                  ),
+                  TextFormField(
+                    decoration: const InputDecoration(label: Text("Name")),
+                    controller: nameController,
+                    validator: (value) {
+                      if(value!.length < 3) {
+                        return 'Name should be at least 3 characters';
+                      } else {
+                        return null;
+                      }
+                    },
+                  ),
+                  const SizedBox(height: 20,),
+                  TextFormField(
+                    decoration: const InputDecoration(label: Text("Email")),
+                    controller: emailController,
+                    validator: (value) {
+                      bool isValid = EmailValidator.validate(value!);
+                      if(!isValid) {
+                        return "Email isn't valid";
+                      } else {
+                        return null;
+                      }
+                    },
+                  ),
+                  const SizedBox(height: 20,),
+                  TextFormField(
+                    decoration: const InputDecoration(label: Text("Password")),
+                    controller: passwordController,
+                    obscureText: true,
+                  ),
+                  const SizedBox(height: 20,),
+                   TextFormField(
+                    decoration: const InputDecoration(label: Text("Confirm Password")),
+                    controller: confirmPasswordController,
+                    obscureText: true,
+                    validator: (value) {
+                      if(value != passwordController.text) {
+                        return "Passwords don't match";
+                      } else {
+                        return null;
+                      }
+                    },
+                  ),
+                  const SizedBox(height: 20,),
+                  ElevatedButton(onPressed: ()=>{
+                    if(formKey.currentState!.validate()){
+                      userRegister()
+                    }
+                  }, child: const Text("Register")),
+                  const SizedBox(height: 20,),
+                  Row(
+                    children: [
+                      const Text("You have an account?"),
+                      TextButton(onPressed: ()=>{
+                        Navigator.push(context,MaterialPageRoute<void>(
+                          builder: (BuildContext context) => const Login(),
+                        ),)
+                      }, child: const Text("Please Login."))
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+  
+  Future<void>userRegister()async {
+    showDialog(context: context, builder: (context) => const Center(
+        child: CircularProgressIndicator(),
+      )
+    );
+
+    try {
+        await FirebaseAuth.instance.createUserWithEmailAndPassword(email: emailController.text, password: passwordController.text);
+        if(mounted) {
+          Navigator.pop(context);
+          Fluttertoast.showToast(
+            msg: "Registered Successfully",
+            toastLength: Toast.LENGTH_SHORT,
+            gravity: ToastGravity.CENTER,
+            timeInSecForIosWeb: 1,
+            backgroundColor: Colors.deepPurple,
+            textColor: Colors.white,
+            fontSize: 16.0
+         );
+      }
+        
+    } on FirebaseException catch(e) {
+      if(mounted) {
+        Navigator.pop(context);
+        Fluttertoast.showToast(
+          msg: e.code,
+          toastLength: Toast.LENGTH_SHORT,
+          gravity: ToastGravity.CENTER,
+          timeInSecForIosWeb: 1,
+          backgroundColor: Colors.deepPurple,
+          textColor: Colors.white,
+          fontSize: 16.0
+        );
+      }
+    }
+  }
+}
